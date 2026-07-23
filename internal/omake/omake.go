af7e1f3908db1d01e718b11bd28b9a1e8e91946d
NikolenkoRostislav <rostislavnikolenkowork@gmail.com>
2026-07-23 20:17:41 +0200

    feat: implement global makefile command

--- internal/omake/omake.go
+++ internal/omake/omake.go
@@ -3,41 +3,39 @@ package omake
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/rostislav/omake/internal/config"
 )
 
-func getMakefileAndConfig() (string, string, error) {
-	configFile, err := config.GetYamlConfigPath()
-	if err != nil {
-		return "", "", err
-	}
-
-	makefileFile, err := config.GetMakefilePath()
-	if err != nil {
-		return "", "", err
-	}
-
-	configData, err := os.ReadFile(configFile)
-	if err != nil {
-		return "", "", err
-	}
-
-	makefileData, err := os.ReadFile(makefileFile)
-	if err != nil {
-		return "", "", err
-	}
-
-	return string(configData), string(makefileData), nil
-}
-
 func Make(command string) {
-	config, makefile, err := getMakefileAndConfig()
+	makefilePath, err := config.GetMakefilePath()
 	if err != nil {
-		fmt.Println("Error:", err.Error())
+		fmt.Println("Error getting Makefile path:", err)
 		return
 	}
 
-	fmt.Println(config, makefile)
-	// TODO: Implement
+	executionDir, err := chooseExecutionDirectory()
+	if err != nil {
+		fmt.Println("Error choosing execution directory:", err)
+		return
+	}
+
+	cmd := exec.Command("make", "-f", makefilePath, "-C", executionDir, command)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func chooseExecutionDirectory() (string, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	return cwd, nil
 }
